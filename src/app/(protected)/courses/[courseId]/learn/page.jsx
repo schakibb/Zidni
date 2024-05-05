6bdf780791032918e80f36da7b4cc8098319e83c
ReD <baslilokmane04@gmail.com>
2024-05-05 01:18:59 +0100

    style : chenged the layout
    in :
      - learn route
      - quiz route
      - completed path

--- src/app/(protected)/courses/[courseId]/learn/page.jsx
+++ src/app/(protected)/courses/[courseId]/learn/page.jsx
@@ -1,7 +1,5 @@
 "use client";
 import * as React from "react";
-import Courses from "../../../../../components/global/Courses/Courses";
-
 import { courses } from "../../../../../data/courses";
 import {
   ResizableHandle,
@@ -9,36 +7,115 @@ import {
   ResizablePanelGroup,
 } from "../../../../../components/ui/resizable";
 import { usePathname } from "next/navigation";
-import Visualisation from "../../../../../components/global/Visualisation";
-
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../../../../../components/ui/card";
+import { cn } from "../../../../../utils/cn";
+import { buttonVariants } from "../../../../../components/ui/button";
+import Link from "next/link";
 const page = () => {
   const pathname = usePathname();
+  const [index, setIndex] = React.useState(1);
   // const selectedCourse = courses.filter(
   //   (course) => course.path === pathname
   // )[0];
   return (
     <>
-      <div className="sm:hidden mt-[18vh]">
+      <div className="sm:hidden">
         <div className="container">
           <p className="text-lg text-center">
             Please Rotate your device for better user experience!
           </p>
         </div>
       </div>
-      <div className="hidden sm:block mt-[12vh] min-h-fit overflow-x-hidden ">
+      <div className="hidden sm:block min-h-fit">
         <ResizablePanelGroup direction="horizontal">
-          <ResizablePanel defaultSize={97}>
-            <Courses courses={courses} />
+          <ResizablePanel defaultSize={35}>
+            <>
+              {courses?.map((course) => (
+                <Card key={course.id} className="m-4 min-w-80">
+                  <CardHeader>
+                    <CardTitle>{course.title}</CardTitle>
+                    <CardDescription>{course.description}</CardDescription>
+                  </CardHeader>
+                  <CardContent>
+                    <div
+                      className={
+                        "flex-col justify-center items-center sm:flex mt-5 w-full"
+                      }
+                    >
+                      {course.chapters.map((chapter, idx) => (
+                        <button
+                          onClick={() => {
+                            setIndex(idx + 1);
+                          }}
+                          className={cn(
+                            "w-full my-1 text-left",
+                            buttonVariants({ variant: "outline" })
+                          )}
+                          key={chapter.chapterId}
+                        >
+                          {idx + 1}. {chapter.title}
+                        </button>
+                      ))}
+                      <Link
+                        href={`/courses/${course.title}/quiz`}
+                        className={cn("my-2", buttonVariants())}
+                      >
+                        Take quiz
+                      </Link>
+                    </div>
+                  </CardContent>
+                </Card>
+              ))}
+            </>
           </ResizablePanel>
-
           <ResizableHandle withHandle className={"min-h-[91dvh]"} />
-          <ResizablePanel className="grid grid-cols-1" defaultSize={3}>
-            <Visualisation />
+          <ResizablePanel defaultSize={65} className="z-[40] w-full">
+            <Visualisation index={index} courses={courses} />
           </ResizablePanel>
         </ResizablePanelGroup>
       </div>
     </>
   );
 };
-
 export default page;
+const Visualisation = ({ courses, index }) => {
+  let idx;
+  idx = index;
+  const filterChapter = (i) => {
+    return courses[0]?.chapters.filter((chapter) => chapter.chapterId == i);
+  };
+  let filteredChapter;
+
+  filteredChapter = filterChapter(idx);
+  return (
+    <div className="min-h-[85dvh] max-h-[83dvh] border-2 rounded-sm m-0 p-0 dark:bg-background overflow-y-scroll">
+      <div className={"flex-col justify-center items-center sm:flex"}>
+        <Card className="m-4">
+          <CardHeader>
+            <CardTitle>
+              {filteredChapter[0]?.chapterId}. {filteredChapter[0]?.title}
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div key={filteredChapter[0]?.chapterId}>
+              {filteredChapter[0]?.subChapter?.map((subChapter) => {
+                return (
+                  <div key={subChapter?.id}>
+                    <p>{subChapter?.subChapterContent}</p>
+                    {subChapter?.subChapterVisualisation}
+                  </div>
+                );
+              })}
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+};
--- src/app/(protected)/courses/[courseId]/quiz/page.jsx
+++ src/app/(protected)/courses/[courseId]/quiz/page.jsx
@@ -1,5 +1,18 @@
+"use client";
+import React from "react";
+import * as storageUnits from "../../../../../data/visualisation/sfsd/storageUnits.json";
+import Lottie, { LottiePlayer, Controls } from "lottie-react";
 const page = () => {
-  return <div>page</div>;
+  return (
+    <>
+      <LottiePlayer src={storageUnits}>
+        <Controls
+          visible={true}
+          buttons={["play", "repeat", "frame", "debug"]}
+        />
+      </LottiePlayer>
+    </>
+  );
 };
 
 export default page;
