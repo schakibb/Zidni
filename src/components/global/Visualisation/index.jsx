f55500d03ed9e5c26860f15d127b7ee9e41a5c72
ReD <baslilokmane04@gmail.com>
2024-05-05 01:19:55 +0100

    feat : sfsd lottie files

--- src/components/global/Visualisation/index.jsx
+++ src/components/global/Visualisation/index.jsx
@@ -1,11 +1,41 @@
 "use client";
-/* TODO: mouloudj mohamed & oussama zitouni*/
 import React from "react";
+import Lottie from "react-lottie";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../../ui/card";
+const VisualisationComponent = ({
+  width = 500,
+  height = 400,
+  animationData,
+  title,
+  description,
+}) => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: false,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
-const Visualisation = () => {
   return (
-    <div className="min-h-[85dvh] max-h-[83dvh] border-2 rounded-sm m-2 p-2 bg-blue-100 border-blue-400"></div>
+    <div className="m-2">
+      <Card>
+        <CardHeader>
+          <CardTitle>Visualisation: {title}</CardTitle>
+        </CardHeader>
+        <CardDescription>{description}</CardDescription>
+        <CardContent>
+          <Lottie options={defaultOptions} height={height} width={width} />
+        </CardContent>
+      </Card>
+    </div>
   );
 };
-
-export default Visualisation;
+export default VisualisationComponent;
