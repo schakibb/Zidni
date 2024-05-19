3e7c0443ba11e749745703bb31208a64f4407bf9
ReD <baslilokmane04@gmail.com>
2024-05-19 10:17:19 +0100

    style : added sheet component

--- src/app/api/courses/route.js
+++ src/app/api/courses/route.js
@@ -1,16 +1,16 @@
-import connectMongoDB from "../../../lib/mongodb";
-import { Module } from "../../../models/models";
-import { NextResponse } from "next/server";
+// import connectMongoDB from "../../../utils/mongodb";
+// import { Module } from "../../../models/models";
+// import { NextResponse } from "next/server";
 
-export async function POST(request) {
-  const { name, semester } = await request.json();
-  await connectMongoDB();
-  await Module.create({ name, semester });
-  return NextResponse.json({ message: "Module Created" }, { status: 201 });
-}
+// export async function POST(request) {
+//   const { name, semester } = await request.json();
+//   await connectMongoDB();
+//   await Module.create({ name, semester });
+//   return NextResponse.json({ message: "Module Created" }, { status: 201 });
+// }
 
-export async function GET() {
-  await connectMongoDB();
-  const modules = await Module.find();
-  return NextResponse.json({ modules });
-}
+// export async function GET() {
+//   await connectMongoDB();
+//   const modules = await Module.find();
+//   return NextResponse.json({ modules });
+// }
