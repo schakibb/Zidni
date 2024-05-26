efce3ca28b83fa45671872aa2ede2a9d31ca93ba
ReD <baslilokmane04@gmail.com>
2024-05-26 02:08:36 +0100

    bug : fixed route default import

--- src/app/api/courses/route.js
+++ src/app/api/courses/route.js
@@ -1,16 +1,4 @@
-// import connectMongoDB from "../../../utils/mongodb";
-// import { Module } from "../../../models/models";
-// import { NextResponse } from "next/server";
-
-// export async function POST(request) {
-//   const { name, semester } = await request.json();
-//   await connectMongoDB();
-//   await Module.create({ name, semester });
-//   return NextResponse.json({ message: "Module Created" }, { status: 201 });
-// }
-
-// export async function GET() {
-//   await connectMongoDB();
-//   const modules = await Module.find();
-//   return NextResponse.json({ modules });
-// }
+export default async function handler(req, res) {
+  const { courseName } = req.query;
+  res.status(200).json({ message: `Quiz for ${courseName}` });
+}
