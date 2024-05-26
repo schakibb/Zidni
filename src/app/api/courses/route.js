78ebcdd096a90a3a104105f49ba2d3e48f166c3c
ReD <baslilokmane04@gmail.com>
2024-05-26 02:21:04 +0100

    bug : fixed import bug

--- src/app/api/courses/route.js
+++ src/app/api/courses/route.js
@@ -1,4 +1,17 @@
-export default async function handler(req, res) {
-  const { courseName } = req.query;
-  res.status(200).json({ message: `Quiz for ${courseName}` });
+// src/app/api/courses/[courseName]/quiz/route.js
+
+import { NextResponse } from "next/server";
+
+export async function GET(request, { params }) {
+  const { courseName } = params;
+  // Your logic to handle the GET request
+  return NextResponse.json({ message: `Quiz for course ${courseName}` });
+}
+
+export async function POST(request, { params }) {
+  const { courseName } = params;
+  // Your logic to handle the POST request
+  return NextResponse.json({
+    message: `Quiz created for course ${courseName}`,
+  });
 }
