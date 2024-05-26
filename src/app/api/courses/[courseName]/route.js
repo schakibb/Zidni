1fac344167ab23a62bc819a7042cfc146c1fcf4c
ReD <baslilokmane04@gmail.com>
2024-05-26 01:59:35 +0100

    deps : removed lottie-players

--- src/app/api/courses/[courseName]/route.js
+++ src/app/api/courses/[courseName]/route.js
@@ -1,18 +1,4 @@
-import connectMongoDB from "../../../../utils/mongodb";
-import { Course } from "../../../../models/models";
-import { NextResponse } from "next/server";
-
-export async function PUT(request, { params }) {
-  const { id } = params;
-  const { newTitle: title, newDescription: description } = await request.json();
-  await connectMongoDB();
-  await Course.findByIdAndUpdate(id, { title, description });
-  return NextResponse.json({ message: "Course updated" }, { status: 200 });
-}
-
-export async function GET(request, { params }) {
-  const { id } = params;
-  await connectMongoDB();
-  const course = await Course.findOne({ _id: id });
-  return NextResponse.json({ course }, { status: 200 });
+export default async function handler(req, res) {
+  const { courseName } = req.query;
+  res.status(200).json({ message: `Quiz for ${courseName}` });
 }
